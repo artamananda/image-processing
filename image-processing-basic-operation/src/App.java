fa7d0e8f0335b3d79cab465d776361cc2d0feac6
Artamananda <artamananda@gmail.com>
2022-09-12 09:25:42 +0700

    add negatif and brigtenning function

--- image-processing-basic-operation/src/App.java
+++ image-processing-basic-operation/src/App.java
@@ -7,7 +7,10 @@ public class App {
         File file = new File("../input/example.jpg");
         int[][] res = converter.Citra.imgToMtx(file);
         int[][] biner = new int[res.length][res[0].length];
-        computing_level.PointLevel.biner(res, biner, 128, res.length, res[0].length);
+
+        // computing_level.PointLevel.biner(res, biner, 128, res.length, res[0].length);
+        computing_level.PointLevel.brightening(res, 2, biner, res.length, res[0].length);
+
         converter.Citra.mtxToImg(biner);
     }
 }
--- image-processing-basic-operation/src/computing_level/PointLevel.java
+++ image-processing-basic-operation/src/computing_level/PointLevel.java
@@ -16,4 +16,36 @@ public class PointLevel{
             }
         }
     }
+
+    public static void negatif(int[][] A, int[][] B, int N, int M){
+        /* Membuat citra negatif dari citra A. Hasilnya disimpan di dalam citra
+        B.
+        Ukuran citra adalah N ´ M.
+        */
+        int i, j;
+        for (i=0; i<=N-1; i++)
+            for (j=0; j<=M-1; j++){
+                B[i][j] = 255 - A[i][j];
+            }
+    }
+
+    public static void brightening(int[][] A, int b, int[][] B, int N, int M){ 
+        /* Pencerahan citra dengan cara menjumlahkan setiap pixel di dalam citra
+        A dengan sebuah skalar b. Hasil disimpan di dalam citra B. Citra A
+        berukuran N ´ M.
+        */
+        int i, j, temp;
+        for (i=0; i<=N-1; i++)
+            for (j=0; j<=M-1; j++){
+                temp = A[i][j] + b;
+                /* clipping */
+                if (temp < 0)
+                    B[i][j] = 0;
+                else
+                if (temp > 255)
+                    B[i][j]=255;
+                else
+                    B[i][j]=temp;
+            }
+    }
 }
