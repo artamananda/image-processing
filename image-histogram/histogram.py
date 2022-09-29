8842ed56f52d1084a3a5103d09b233b608aeadad
Artamananda <artamananda@gmail.com>
2022-09-29 14:41:03 +0700

    delete input.jpg and add some comment in histogram.py

--- image-histogram/histogram.py
+++ image-histogram/histogram.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 a = np.array(Image.open(r"C:\Users\Arta's Windows\Pictures\Screenshots\input.jpg"))
+# you can change path to your image path
 x = np.ravel(a)
 
 plt.hist(x)
