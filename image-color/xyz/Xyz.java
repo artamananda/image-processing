888821d910b88f2e070e57b0be2c3ad75b20432b
Artamananda <artamananda@gmail.com>
2022-11-09 10:24:58 +0700

    add xyz to rgb method

--- image-color/xyz/Xyz.java
+++ image-color/xyz/Xyz.java
@@ -10,7 +10,8 @@ public class Xyz {
         int[][] mtxImgRed = imgToMtxRed(file);
         int[][] mtxImgGreen = imgToMtxGreen(file);
         int[][] mtxImgBlue = imgToMtxBlue(file);
-        cieRGB_toXYZ(mtxImgRed, mtxImgGreen, mtxImgBlue);
+        //cieRGB_toXYZ(mtxImgRed, mtxImgGreen, mtxImgBlue);
+        XYZ_to_cieRGB(mtxImgRed, mtxImgGreen, mtxImgBlue);
     }
 
     public static void cieRGB_toXYZ(int[][] r, int[][] g, int[][] b) throws IOException{
@@ -34,6 +35,35 @@ public class Xyz {
                 else z[i][j]=(int)Z;
             }
 
+        mtxToImg(x, y, z);
+    }
+
+    public static void XYZ_to_cieRGB(int[][] x, int[][] y, int[][] z) throws IOException{
+        int N = x.length;
+        int M = x[0].length;
+        int[][] r = new int[N][M];
+        int[][] g = new int[N][M];
+        int[][] b = new int[N][M];
+        int i, j; double R, G, B; double X, Y, Z;
+        for (i=0; i<=N-1; i++)
+            for (j=0; j<=M-1; j++){
+                X = (double)x[i][j]; Y =(double)y[i][j]; Z =(double)z[i][j];
+                R = 2.365*X-0.896*Y-0.468*Z;
+                G = -0.515*X+1.425*Y+0.088*Z;
+                B = 0.005*X-0.014*Y+1.009*Z;
+                if (R > 255.0) r[i][j]=255;
+                else if (R<0.0) r[i][j]=0;
+                else r[i][j]=(int)R;
+
+                if (G > 255.0) g[i][j]=255;
+                else if (G<0.0) g[i][j]=0;
+                else g[i][j]=(int)G;
+
+                if (B > 255.0) b[i][j]=255;
+                else if (B<0.0) b[i][j]=0;
+                else b[i][j]=(int)B;
+            }
+        
         mtxToImg(x, y, z);
     }
 
